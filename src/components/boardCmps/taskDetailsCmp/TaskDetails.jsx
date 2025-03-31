ee0f845b436514ff2aedbebaf2bb655af7c00153
senpaiharde <xxslavan1@gmail.com>
2025-03-31 03:43:12 +0300

    full svg paths to sidebar and all the buttons/ icons/ splited everything between folders

--- src/components/boardCmps/taskDetailsCmp/TaskDetails.jsx
+++ src/components/boardCmps/taskDetailsCmp/TaskDetails.jsx
@@ -17,6 +17,8 @@ import TaskDetailsLabel from "./TaskDetailsLabel";
 import TaskDetailsNotifcations from "./TaskDetailsNotifcations";
 import TaskDetailsDate from "./TaskDetailsDate";
 import TaskDetailsActivity from "./main/TaskDetailsActivity";
+import TaskDetailsSidebar from "./sidebar/TaskDetailsSidebar";
+import TaskDescription from "./main/TaskDetailsDescription";
 const TaskDetails = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -105,14 +107,7 @@ const TaskDetails = () => {
             <TaskDetailsDate/>
 
             </div>
-            <div className="td-section">
-              <div className="td-section-header">Description</div>
-              <textarea
-                className="td-description"
-                value={selectedTask.taskDescription || ""}
-                onChange={handleDescriptionChange}
-              />
-            </div>
+            <TaskDescription/>
 
             <div className="td-section">
               <div className="td-section-header">Checklist</div>
@@ -138,53 +133,10 @@ const TaskDetails = () => {
               </button>
             </div>
 
-            <TaskDetailsActivity/>
+            <TaskDetailsActivity />
           </div>
 
-          <div className="td-sidebar">
-          <button className="trello-btn"><User size={16} /> Leave</button>
-        <button className="trello-btn"><User size={16} /> Members</button>
-        <button className="trello-btn"><Tag size={16} /> Labels</button>
-        <button className="trello-btn"><Calendar size={16} /> Dates</button>
-        <button className="trello-btn"><Paperclip size={16} /> Attachment</button>
-        <button className="trello-btn"><MapPin size={16} /> Location</button>
-        <button className="trello-btn"><Image size={16} /> Cover</button>
-        <button className="trello-btn"><Settings size={16} /> Custom Fields</button>
-
-            <section className="td-sidebar">
-                <hgroup>
-                    <h4 className="td-sidebar-power">Power-Ups
-                    </h4>
-                </hgroup>
-                
-                    <a className="trello-btn" href="" type="button"> <span className="trello-btn-text">Add Power-Ups</span>
-                        <span className="td-icons"><svg width={24} height={24}><path d="M12 3C11.4477 3 11 3.44772 11 4V11L4 11C3.44772 11 3 11.4477 3 12C3 12.5523 3.44772 13 4 13H11V20C11 20.5523 11.4477 21 12 21C12.5523 21 13 20.5523 13 20V13H20C20.5523 13 21 12.5523 21 12C21 11.4477 20.5523 11 20 11L13 11V4C13 3.44772 12.5523 3 12 3Z"></path></svg></span>
-                    </a>
-               
-            </section>
-            <section className="td-sidebar">
-                <hgroup>
-                    <h4 className="td-sidebar-power">Automation
-                        <button className="td-sidebar-h4-button"></button>
-                    </h4>
-                </hgroup>
-                
-                    <a className="trello-btn" href="" type="button"> <span className="trello-btn-text">Add button</span>
-                        <span className="td-icons"><svg width={24} height={24}><path d="M12 3C11.4477 3 11 3.44772 11 4V11L4 11C3.44772 11 3 11.4477 3 12C3 12.5523 3.44772 13 4 13H11V20C11 20.5523 11.4477 21 12 21C12.5523 21 13 20.5523 13 20V13H20C20.5523 13 21 12.5523 21 12C21 11.4477 20.5523 11 20 11L13 11V4C13 3.44772 12.5523 3 12 3Z"></path></svg></span>
-                    </a>
-               
-            </section>
-
-             <div className="section-header">Actions</div>
-             <button className="trello-btn"><Copy size={16} /> Move</button>
-            <button className="trello-btn"><Copy size={16} /> Copy</button>
-            <button className="trello-btn"><Copy size={16} /> Mirror</button>
-            <button className="trello-btn"><Copy size={16} /> Copy</button>
-            <button className="trello-btn"><Copy size={16} /> Make template</button>
-            <div className="divider" ></div>
-            <button className="trello-btn"><Archive size={16} /> Archive</button>
-            <button className="trello-btn"><Share size={16} /> Share</button>
-            </div>
+          <TaskDetailsSidebar/>
         </div>
       </div>
     </div>
--- src/components/boardCmps/taskDetailsCmp/TaskDetailsLabel.jsx
+++ src/components/boardCmps/taskDetailsCmp/TaskDetailsLabel.jsx
@@ -2,7 +2,7 @@
 
 
 import React from "react";
-
+import '../../../styles/taskDetailsFolder/TaskDetailsLabel.scss';
 const TaskDetailsLabel = () => {
     const labels = [
         { title: "dsgsg", color: "orange" },
@@ -27,7 +27,7 @@ const TaskDetailsLabel = () => {
                         minWidth: '48px',
                         maxWidth: '100%',
                         marginBottom: 0,
-                        padding: '0 8px',
+                        padding: '15px 8px',
                         overflow: 'hidden',
                         borderRadius: '3px',
                         fontSize: '13px',
@@ -49,15 +49,39 @@ const TaskDetailsLabel = () => {
                           }}>
 
               </span>
-                        <div style={{ marginLeft: '8px' }}>{template.title}</div>
+              <div style={{ marginLeft: '8px', zIndex: 1 }}>{template.title}</div>
                     </div>
                 ))}
-                <button className="td-section-members-add"
-                
-                
-                >
-                    <img alt="add Label" style={{ width: '24px', height: '24px' }}/>
-                </button>
+                <button
+          className="td-section-members-add"
+          style={{
+            marginRight: '8px',
+            padding: '3px 4px',
+            border: 'none',
+            justifyContent: 'center',
+            display: 'inline-flex',
+            height: '30px',
+            width: '30px',
+            lineHeight: '14px',
+            fontSize: '10px',
+            alignItems: 'center',
+            borderRadius: '3px'
+          }}
+        >
+          <svg
+            width="14px"
+            height="14px"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              display: "flex",
+            }}
+          >
+            <path d="M12 3C11.4477 3 11 3.44772 11 4V11L4 11C3.44772 11 3 11.4477 3 12C3 12.5523 3.44772 13 4 13H11V20C11 20.5523 11.4477 21 12 21C12.5523 21 13 20.5523 13 20V13H20C20.5523 13 21 12.5523 21 12C21 11.4477 20.5523 11 20 11L13 11V4C13 3.44772 12.5523 3 12 3Z" />
+          </svg>
+        </button>
             </div>
         </section>
     );
