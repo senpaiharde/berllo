b230bd3371f929ba977ac6eb4489f566094dd4e4
senpaiharde <xxslavan1@gmail.com>
2025-04-15 20:41:21 +0300

    full ready notification button missing only global redux but ui ready

--- src/components/boardCmps/taskDetailsCmp/TaskDetailsNotifcations.jsx
+++ src/components/boardCmps/taskDetailsCmp/TaskDetailsNotifcations.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, {  useState } from "react";
 import '../../../styles/taskDetailsFolder/TaskDetailsNotifications.scss';
 import SvgWatching from "../../../assets/svgDesgin/SvgWatching";
 import SvgCheck from "../../../assets/svgDesgin/SvgCheck";
 const TaskDetailsNotifcations = () => {
+    const [isWatching, setIsWatching] = useState(false);
+
 
     return (
         <section className="td-section-top-section">
         <h3 className="td-section-top-h3">Notifications</h3>
         <div>
-          <button className="notification-button1"
-            
-          >
+            {isWatching ? (
+                <button style={{width:'88px'}}
+                onClick={() => setIsWatching(prev => !prev)} className="notification-button1">
+            <span className="notification-button-span">
+              <span className="notification-button-span">
+                <SvgWatching/>
+              </span>
+            </span>
+            Watch
+            <span className="notification-button-span"
+              style={{
+                marginLeft: "-5px",
+              }}
+            >
+              <span  className="notification-button-span">
+                {isWatching ? '' : <SvgCheck/>}
+              </span>
+            </span>
+          </button>) : (<button  
+          onClick={() => setIsWatching(prev => !prev)} className="notification-button1">
             <span className="notification-button-span">
               <span className="notification-button-span">
                 <SvgWatching/>
@@ -23,10 +42,11 @@ const TaskDetailsNotifcations = () => {
               }}
             >
               <span  className="notification-button-span">
-                <SvgCheck/>
+               <SvgCheck/>
               </span>
             </span>
-          </button>
+          </button>)}
+          
         </div>
       </section>
     );
