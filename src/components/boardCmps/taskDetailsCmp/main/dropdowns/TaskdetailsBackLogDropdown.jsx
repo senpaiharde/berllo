2533debc33542f6d8ae069f5ec8888bc7ccc27d2
senpaiharde <xxslavan1@gmail.com>
2025-04-18 19:12:53 +0300

    move card first look added label with test h4 and styles and div that will act as extra dropdown for selection

--- src/components/boardCmps/taskDetailsCmp/main/dropdowns/TaskdetailsBackLogDropdown.jsx
+++ src/components/boardCmps/taskDetailsCmp/main/dropdowns/TaskdetailsBackLogDropdown.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import SvgClose from '../../../../../assets/svgDesgin/SvgClose';
+import SvgDropdown from '../../../../../assets/svgDesgin/SvgDate/SvgDropdown';
 
 const TaskdetailsBackLogDropdown = ({ trigger, onClose }) => {
     const dispatch = useDispatch();
@@ -16,7 +17,7 @@ const TaskdetailsBackLogDropdown = ({ trigger, onClose }) => {
     <div className="DropdownUi">
       {/* Header */}
       <div className="DropdownUiHeader">
-        <h2 className="DropdownHeaderH2">Members</h2>
+        <h2 className="DropdownHeaderH2">Move Card</h2>
         <button className="DropdownClose" onClick={onClose}>
           <SvgClose />
         </button>
@@ -24,7 +25,20 @@ const TaskdetailsBackLogDropdown = ({ trigger, onClose }) => {
 
       {/* Options */}
       <div className="DropdownOptions" style={{}}>
-        
+        <h4 className='WorkflowAreah4'>Select destination</h4>
+        <div className='WorkflowArea'>
+            <label className='WorkflowAreaLabel'>Board</label>
+            <div className="BoardReminderDiv">
+                  <div className="BoardReminderDivText">
+                    <div className="BoardReminderDivText2"> title</div>
+                  </div>
+                  <div className="BoardReminderDivSVG">
+                    <span className="BoardReminderDivSVG2">
+                      <SvgDropdown />
+                    </span>
+                  </div>
+                </div>
+        </div>
         
 
         
