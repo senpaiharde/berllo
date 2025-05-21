f5824d785c1bcec9d6b3bc7825984ea88d4fe609
senpaiharde <xxslavan1@gmail.com>
2025-05-21 17:21:58 +0300

    added more clear styles + color and style for checklist bar

--- src/utils/ProgressBar.jsx
+++ src/utils/ProgressBar.jsx
@@ -4,12 +4,15 @@
 import React from "react"
 
 const ProgressBar = ({completionPercentage}) => {
+
     return(
         <>
-        <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-start", marginTop: "6px" ,width:'545px',marginBottom:'3px' }}>
-            <div style={{ marginRight: "15px",marginLeft:'1px', fontSize: "11px" }}>{completionPercentage.toFixed(0)}%</div>
+        <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-start", marginTop: "6px" ,width:'545px',marginBottom:'3px' ,marginRight:'16px'}}>
+            <div style={{ paddingRight:'4px',marginLeft:'1px', fontSize: "11px" }}>{completionPercentage.toFixed(0)}%</div>
             <div style={{ width: "100%", height: "5px", backgroundColor: "#e0e0e0", borderRadius: "4px", maxWidth: "100%" }}>
-              <div style={{ width: `${completionPercentage}%`, height: "7px", backgroundColor: "#44546F", borderRadius: "4px",
+              <div style={{ width: `${completionPercentage}%`, height: "6px", backgroundColor: completionPercentage === 100 ? "#1f845a" :  "#44546F",
+              
+              borderRadius: "4px",
                transition: "width 0.3s ease-in-out" }} />
             </div>
           </div>
