32e05e31f6ae107d460a57ddf15bcafa24be102a
senpaiharde <xxslavan1@gmail.com>
2025-05-28 21:20:49 +0300

    added basic modal at left at homepage, now working on middle

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -29,15 +29,54 @@ export function Home() {
             </ul>
           </div>
           <div>
-            <ul>
-                <li><a></a>
-                <ul></ul>
-                <div></div></li>
-              
+            <ul className="HomepageDisplayNavBottom">
+              <div className="HomepageDisplayNavBottomText"></div>
+              <li className="HomepageDisplayNavTopButtons">
+                <a className="HomepageDisplayNavBottomWorkspace"></a>
+                <ul className="HomepageDisplayNavBottomWorkspaceUL">
+                  <li className="HomepageDisplayNavBottomWorkspaceULLi">
+                    <a>
+                      <span></span>
+                      <span>Boards</span>
+                    </a>
+                  </li>
+                  <li className="HomepageDisplayNavBottomWorkspaceULLi">
+                    <a>
+                      <span></span>
+                      <span>Highlights</span>
+                    </a>
+                  </li>
+                  <li className="HomepageDisplayNavBottomWorkspaceULLi">
+                    <a>
+                      <span></span>
+                      <span>Views</span>
+                    </a>
+                  </li>
+                  <li className="HomepageDisplayNavBottomWorkspaceULLi">
+                    <a>
+                      <span></span>
+                      <span>Members</span>
+                    </a>
+                  </li>
+                  <li className="HomepageDisplayNavBottomWorkspaceULLi">
+                    <a>
+                      <span></span>
+                      <span>Settings</span>
+                    </a>
+                  </li>
+                </ul>
+              </li>
             </ul>
           </div>
         </nav>
-        <div className="home-main-content-container"></div>
+        <div className="home-main-content-container">
+          <div 
+            className="home-main-content-item">
+                <div className="home-main-content-item-high"></div>
+                <div></div>
+                <ul></ul>
+            </div>
+        </div>
         <div className="home-right-sidebar-container"></div>
       </div>
     </div>
