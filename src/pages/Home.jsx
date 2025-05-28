6fd67693efdffc25db84efdefaf44953047db038
senpaiharde <xxslavan1@gmail.com>
2025-05-28 22:37:28 +0300

    add svgs worked on left side of home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,34 @@
+import { useNavigate } from 'react-router-dom';
 import GlobalHeader from '../components/GlobalHeader';
+import BoardsIcon from '.././assets/images/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHJvbGU9InByZXNlbnRhdGlvbiIgZm9jdXNhYmxlPSJmYWxzZSIgdmlld0JveD0iMCAwIDI0IDI0IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCI (2).svg';
+import myIconSearch from '.././assets/images/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0IiByb2xlPSJwcmVzZW50YXRpb24iPjxwYXRoIGZpbGw9ImN1cnJlbnRjb2xvciIgZmlsbC1ydWxlPSJldmVub2RkIiB.svg';
+
+import homeIcon from '.././assets/images/svg+xml;base64,PHN2ZyB3aWR0aD0iMjQiIGhlaWdodD0iMjQiIHJvbGU9InByZXNlbnRhdGlvbiIgZm9jdXNhYmxlPSJmYWxzZSIgdmlld0JveD0iMCAwIDI0IDI0IiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciPjxwYXRoIGZpbGwtcnVsZT0iZXZlbm9kZCI (3).svg';
+const workspaceLeft = [
+  {
+    title: 'Boards',
+    icon: BoardsIcon,
+  },
+  {
+    title: 'highlights',
+    icon: '',
+  },
+  {
+    title: 'Views',
+    icon: homeIcon,
+  },
+  {
+    title: 'Members',
+    icon: '',
+  },
+  {
+    title: 'settings',
+    icon: '',
+  },
+];
 
 export function Home() {
+  const navigate = useNavigate();
   return (
     <div>
       <GlobalHeader />
@@ -9,75 +37,139 @@ export function Home() {
           <div>
             <ul className="HomepageDisplayNavTop">
               <li className="HomepageDisplayNavTopButtons">
-                <a>
-                  <span></span>
-                  <span>Boards</span>
+                <a className="HomepageDisplayNavTopButtonsHref">
+                  <span className="HomepageDisplayNavTopButtonsIcon">
+                    <span className="HomepageDisplayNavTopButtonsIconInside">
+                      <img
+                        style={{ marginTop: '4px' }}
+                        width={16}
+                        height={16}
+                        src={svgsHome}
+                        alt="My icon"
+                      />
+                    </span>
+                  </span>
+                  <span className="HomepageDisplayNavTopButtonsBoard">Boards</span>
                 </a>
               </li>
               <li className="HomepageDisplayNavTopButtons">
-                <a>
-                  <span></span>
-                  <span>Templates</span>
+                <a className="HomepageDisplayNavTopButtonsHref">
+                  <span className="HomepageDisplayNavTopButtonsIcon">
+                    <span className="HomepageDisplayNavTopButtonsIconInside">
+                      <img
+                        style={{ marginTop: '4px', color: '#172b4d' }}
+                        width={16}
+                        height={16}
+                        src={myIconSearch}
+                        alt="My icon"
+                      />
+                    </span>
+                  </span>
+                  <span className="HomepageDisplayNavTopButtonsBoard">Templates</span>
                 </a>
               </li>
               <li className="HomepageDisplayNavTopButtons">
-                <a>
-                  <span></span>
-                  <span>Home</span>
+                <a className="HomepageDisplayNavTopButtonsHref">
+                  <span className="HomepageDisplayNavTopButtonsIcon">
+                    <span className="HomepageDisplayNavTopButtonsIconInside">
+                      <img
+                        style={{ marginTop: '4px' }}
+                        width={16}
+                        height={16}
+                        src={homeIcon}
+                        alt="My icon"
+                      />
+                    </span>
+                  </span>
+                  <span className="HomepageDisplayNavTopButtonsBoard" onClick={() => navigate('/')}>
+                    Home
+                  </span>
                 </a>
               </li>
             </ul>
           </div>
           <div>
             <ul className="HomepageDisplayNavBottom">
-              <div className="HomepageDisplayNavBottomText"></div>
+              <div className="HomepageDisplayNavBottomText">
+                <div className="HomepageDisplayNavBottomText">
+                  <h2 className="HomepageDisplayNavBottomTextH2">Workspaces</h2>
+                </div>
+              </div>
               <li className="HomepageDisplayNavTopButtons">
-                <a className="HomepageDisplayNavBottomWorkspace"></a>
+                <a className="HomepageDisplayNavBottomWorkspace">
+                  <div className="HomepageDisplayNavBottomWorkspaceIcon"></div>
+                  <span className="HomepageDisplayNavBottomWorkspaceIconspan">
+                    Trello Workspace
+                  </span>
+                  <span className="HomepageDisplayNavBottomWorkspaceIconSvg"> </span>
+                </a>
                 <ul className="HomepageDisplayNavBottomWorkspaceUL">
-                  <li className="HomepageDisplayNavBottomWorkspaceULLi">
-                    <a>
-                      <span></span>
-                      <span>Boards</span>
-                    </a>
-                  </li>
-                  <li className="HomepageDisplayNavBottomWorkspaceULLi">
-                    <a>
-                      <span></span>
-                      <span>Highlights</span>
-                    </a>
-                  </li>
-                  <li className="HomepageDisplayNavBottomWorkspaceULLi">
-                    <a>
-                      <span></span>
-                      <span>Views</span>
-                    </a>
-                  </li>
-                  <li className="HomepageDisplayNavBottomWorkspaceULLi">
-                    <a>
-                      <span></span>
-                      <span>Members</span>
-                    </a>
-                  </li>
-                  <li className="HomepageDisplayNavBottomWorkspaceULLi">
-                    <a>
-                      <span></span>
-                      <span>Settings</span>
-                    </a>
-                  </li>
+                  {workspaceLeft.map((item) => (
+                    <li className="HomepageDisplayNavTopButtons">
+                      <a
+                        className="HomepageDisplayNavTopButtonsHref"
+                        style={{ color: '#172b4d', fontWeight: '200' }}>
+                        <span className="HomepageDisplayNavTopButtonsIcon">
+                          <span className="HomepageDisplayNavTopButtonsIconInside">
+                            <img
+                              style={{ marginTop: '4px', color: '#172b4d' }}
+                              width={16}
+                              height={16}
+                              src={item.icon}
+                              alt="My icon"
+                            />
+                          </span>
+                        </span>
+                        <span className="HomepageDisplayNavTopButtonsBoard">{item.title}</span>
+                      </a>
+                    </li>
+                  ))}
                 </ul>
               </li>
             </ul>
           </div>
         </nav>
         <div className="home-main-content-container">
-          <div 
-            className="home-main-content-item">
-                <div className="home-main-content-item-high"></div>
-                <div></div>
-                <ul></ul>
+          <div className="home-main-content-item">
+            <div className="home-main-content-item-high">
+              <div></div>
+              <div className="home-main-content-item-Highlights"> Highlights</div>
             </div>
+            <div className="home-main-content-item-high-container">
+              <div></div>
+              <div className="home-main-content-item-high-container-text">
+                <span className="home-main-content-item-high-container-text-first">Highlights</span>
+                <span className="home-main-content-item-high-container-text-2">
+                  Stay up to date with activity from your Workspaces and boards.
+                </span>
+                <button className="home-main-content-item-high-container-button">
+                  Got it! Dismiss this.
+                </button>
+              </div>
+            </div>
+            <ul className="home-main-content-container-ul">
+              <li className="home-main-content-container-ul-li"></li>
+              <li></li>
+            </ul>
+          </div>
         </div>
-        <div className="home-right-sidebar-container"></div>
+        <div className="home-right-sidebar-container">
+          <div style={{ paddingBottom: '24px' }}>
+            <div className="home-right-sidebar-container-viewed"></div>
+            <ul className="home-right-sidebar-container-viewed-ul"></ul>
+          </div>
+          <div style={{ paddingBottom: '24px' }}>
+            <div className="home-right-sidebar-container-link">
+              <div className="home-right-sidebar-container-link-inside"></div>
+            </div>
+            <div className="home-right-sidebar-container-create">
+              <button className="home-right-sidebar-container-bottom">
+                <span className="home-right-sidebar-container-bottom-plus">+</span>
+                <span>Create a board</span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
