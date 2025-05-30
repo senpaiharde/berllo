f620e7afaa721d51545c98327e7be1fc2a1c8f49
senpaiharde <xxslavan1@gmail.com>
2025-05-30 23:59:49 +0300

    section to add board ready

--- src/pages/BoardsCreateDropdown.jsx
+++ src/pages/BoardsCreateDropdown.jsx
@@ -6,21 +6,58 @@ import { useRef } from 'react';
 import { SvgServices } from '../services/svgServices';
 import { syncTaskAsync } from '../redux/TaskDetailsSlice';
 import { TaskOps } from '../services/backendHandler';
+import { useNavigate } from 'react-router-dom';
+import { fetchCurrentBoard } from '../services/backendCallsUsers';
 
 const BoardsCreateDropdown = ({ onClose }) => {
-  
+  const navigate = useNavigate();
   const dispatch = useDispatch();
-const [boardTitle, setBoardTitle] = useState(null);
-function createNewboard() {
-        // dispatch(addnewBoard(`new board ${currentWorkSpace.boards?.length}`))
-        dispatch(syncTaskAsync({ 
-            method: TaskOps.ADD,
-            args: {
-              body: { method: TaskOps.ADD, workId: "board", boardTitle: `${boardTitle}` },
-            },
-            workId: "board",
-          }))
+  const [workSpace, setWorkSpace] = useState();
+  const [boardTitle, setBoardTitle] = useState(null);
+  useEffect(() => {
+    async function load() {
+      try {
+        const boards = await fetchCurrentBoard();
+        console.log(boards.boards);
+        setWorkSpace(boards.boards);
+      } catch (err) {
+        console.log('there is error on loading users', err);
+        return err;
       }
+    }
+    console.log(workSpace?.boards, 'yes');
+    load();
+  }, []);
+
+  function createNewboard() {
+    // dispatch(addnewBoard(`new board ${currentWorkSpace.boards?.length}`))
+    dispatch(
+      syncTaskAsync({
+        method: TaskOps.ADD,
+        args: {
+          body: { method: TaskOps.ADD, workId: 'board', boardTitle: `${boardTitle}` },
+        },
+        workId: 'board',
+      })
+    );
+    async function load() {
+      try {
+        const boards = await fetchCurrentBoard();
+        console.log(boards.boards);
+        setWorkSpace(boards.boards);
+      } catch (err) {
+        console.log('there is error on loading users', err);
+        return err;
+      }
+    }
+    console.log(workSpace?.boards, 'yes');
+    load();
+    const lastBoard = workSpace[Object.keys(workSpace)[Object.keys(workSpace).length - 1]];
+    console.log(lastBoard._id, 'here im ');
+    setTimeout(() => {
+      navigate(`/b/${lastBoard._id}/${boardTitle}`);
+    }, 500);
+  }
   return (
     <div className="DropdownUi">
       {/* Header */}
@@ -30,58 +67,59 @@ function createNewboard() {
           <SvgServices name="SvgClose" />
         </button>
       </div>
-     
+
       {/* Options */}
-      <div style={{grid:'none'}} className="DropdownLabelOption">
-        <div className='ImgDropdown'>
-            <div className='ImgDropdownInside'>
-           
-        </div>
+      <div style={{ grid: 'none' }} className="DropdownLabelOption">
+        <div className="ImgDropdown">
+          <div className="ImgDropdownInside"></div>
         </div>
         <h3 className="DropdownLabelH3">Background</h3>
         <h3 className="DropdownLabelH3">Board title</h3>
-       <input
-            onChange={(e) => setBoardTitle(e.target.value)}
+        <input
+          required
+          onChange={(e) => setBoardTitle(e.target.value)}
           value={boardTitle}
-          className="EditDropdownInput"
-          style={{ paddingLeft: '13px',height:'36px' }}
+          className="EditDropdownInputBoard"
+          style={{}}
           placeholder="Search labels..."
         />
+        <p className="TextBoardDropdown">
+          <span style={{ marginRight: '4px' }}>👋</span> Board title is required
+        </p>
         <h3 className="DropdownLabelH3">Workspace</h3>
-        <label 
-                        className="BoardReminder"
-                      >
-                        Set due date reminder
-                        <div className="BoardReminderDiv">
-                          <div className="BoardReminderDivText">
-                            <div className="BoardReminderDivText2">Berllo Workspace</div>
-                          </div>
-                          <div className="BoardReminderDivSVG">
-                            <span className="BoardReminderDivSVG2">
-                              <SvgServices name="SvgDropdown" />
-                            </span>
-                          </div>
-                        </div>
-                      </label>
+        <label className="BoardReminder">
+          Set due date reminder
+          <div className="BoardReminderDiv">
+            <div className="BoardReminderDivText">
+              <div className="BoardReminderDivText2">Berllo Workspace</div>
+            </div>
+            <div className="BoardReminderDivSVG">
+              <span className="BoardReminderDivSVG2">
+                <SvgServices name="SvgDropdown" />
+              </span>
+            </div>
+          </div>
+        </label>
         <h3 className="DropdownLabelH3">visibility</h3>
-        <label 
-                        className="BoardReminder"
-                      >
-                        Set due date reminder
-                        <div className="BoardReminderDiv">
-                          <div className="BoardReminderDivText">
-                            <div className="BoardReminderDivText2">Workspace</div>
-                          </div>
-                          <div className="BoardReminderDivSVG">
-                            <span className="BoardReminderDivSVG2">
-                              <SvgServices name="SvgDropdown" />
-                            </span>
-                          </div>
-                        </div>
-                      </label>
-                      <button style={{marginTop:'10px'}} onClick={() => (true)} className="DropdownLabelButton">
-              Create
-            </button>
+        <label className="BoardReminder">
+          Set due date reminder
+          <div className="BoardReminderDiv">
+            <div className="BoardReminderDivText">
+              <div className="BoardReminderDivText2">Workspace</div>
+            </div>
+            <div className="BoardReminderDivSVG">
+              <span className="BoardReminderDivSVG2">
+                <SvgServices name="SvgDropdown" />
+              </span>
+            </div>
+          </div>
+        </label>
+        <button
+          style={{ marginTop: '10px' }}
+          onClick={() => createNewboard()}
+          className="DropdownLabelButton">
+          Create
+        </button>
       </div>
     </div>
   );
