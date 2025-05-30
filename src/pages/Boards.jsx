125d3b4e178b7ca640b0202ac305552615d8b2f3
senpaiharde <xxslavan1@gmail.com>
2025-05-30 21:25:38 +0300

    rework for boards section

--- src/pages/Boards.jsx
+++ src/pages/Boards.jsx
@@ -194,193 +194,61 @@ export function Boards() {
                 </span>
                 <h3 className="BoardsHomeContainerMainRecentTitleh3">Recently viewed</h3>
               </div>
-            
-                <div
-              
-                className="BoardsHomeContainerMainRecentDisplay">
-                     {user?.lastBoardVisited[0]?.boardTitle && (
-                  <div 
-                    onClick={() => {
-                        const last = user?.lastBoardVisited?.[0];
 
-                        const slug = last?.boardTitle;
-                        const boardId = last?.id;
+              <div className="BoardsHomeContainerMainRecentDisplay">
+                {user?.lastBoardVisited?.map((recent) => {
+                     const { id, boardTitle } = recent;
+                  const starEntry = user.starredBoards?.find((sb) => sb.id === id);
+                  const isStarred = !!starEntry?.isStarred;
+                  return (
+                    <div
+                      onClick={() => {
+                        
 
-                        console.log('last visited entry:', last);
-                        console.log('🧠 Navigating to:', `/b/${boardId}/${slug}`);
+                        
 
-                        if (boardId && slug) {
-                          navigate(`/b/${boardId}/${slug}`);
-                        }
+                        console.log('last visited entry:', recent);
+                        console.log('🧠 Navigating to:', `/b/${id}/${boardTitle}`);
+
+                        
+                          navigate(`/b/${id}/${boardTitle}`);
                       }}
-                  className="DisplayCardsInBoards">
-                    <a className="DisplayCardsInBoardsA">
-                      <div className="DisplayCardsInBoardsADiv"></div>
-                      <div className="DisplayCardsInBoardsADiv2">
-                        <div className="DisplayCardsInBoardsADiv2Title">{user?.lastBoardVisited[0]?.boardTitle}</div>
-                        <div className="DisplayCardsInBoardsADiv2Svg">
-                          <div className="DisplayCardsInBoardsADiv2SvgIcon">
-                            <div className="DisplayCardsInBoardsADiv2SvgIconDiv">
-                              <span className="DisplayCardsInBoardsADiv2SvgIconSpan">
-                                <svg
-                                  style={{ color: 'white' }}
-                                  viewBox="0 0 24 24"
-                                  focusable="false"
-                                  role="presentation"
-                                  width="16"
-                                  height="16">
-                                  <path
-                                    d="M12.5048 5.67168C11.9099 5.32669 11.2374 5.10082 10.5198 5.0267C11.2076 3.81639 12.5085 3 14 3C16.2092 3 18 4.79086 18 7C18 7.99184 17.639 8.89936 17.0413 9.59835C19.9512 10.7953 22 13.6584 22 17C22 17.5523 21.5523 18 21 18H18.777C18.6179 17.2987 18.3768 16.6285 18.0645 16H19.917C19.4892 13.4497 17.4525 11.445 14.8863 11.065C14.9608 10.7218 15 10.3655 15 10C15 9.58908 14.9504 9.18974 14.857 8.80763C15.5328 8.48668 16 7.79791 16 7C16 5.89543 15.1046 5 14 5C13.4053 5 12.8711 5.25961 12.5048 5.67168ZM10 12C11.1046 12 12 11.1046 12 10C12 8.89543 11.1046 8 10 8C8.89543 8 8 8.89543 8 10C8 11.1046 8.89543 12 10 12ZM14 10C14 10.9918 13.639 11.8994 13.0412 12.5984C15.9512 13.7953 18 16.6584 18 20C18 20.5523 17.5523 21 17 21H3C2.44772 21 2 20.5523 2 20C2 16.6584 4.04879 13.7953 6.95875 12.5984C6.36099 11.8994 6 10.9918 6 10C6 7.79086 7.79086 6 10 6C12.2091 6 14 7.79086 14 10ZM9.99999 14C12.973 14 15.441 16.1623 15.917 19H4.08295C4.55902 16.1623 7.02699 14 9.99999 14Z"
-                                    fill-rule="evenodd"
-                                    clip-rule="evenodd"></path>
-                                </svg>
-                              </span>
-                            </div>
+                     
+                      className="DisplayCardsInBoards">
+                      <a className="DisplayCardsInBoardsA">
+                        <div className="DisplayCardsInBoardsADiv"></div>
+                        <div className="DisplayCardsInBoardsADiv2">
+                          <div className="DisplayCardsInBoardsADiv2Title">
+                            {boardTitle}
                           </div>
-                          <div className="DisplayCardsInBoardsADiv2SvgBlock"></div>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                   )}
-                   {user?.lastBoardVisited[1]?.boardTitle && (
-                  <div 
-                    onClick={() => {
-                        const last = user?.lastBoardVisited?.[1];
-
-                        const slug = last?.boardTitle;
-                        const boardId = last?.id;
-
-                        console.log('last visited entry:', last);
-                        console.log('🧠 Navigating to:', `/b/${boardId}/${slug}`);
-
-                        if (boardId && slug) {
-                          navigate(`/b/${boardId}/${slug}`);
-                        }
-                      }}
-                  className="DisplayCardsInBoards">
-                    <a className="DisplayCardsInBoardsA">
-                      <div className="DisplayCardsInBoardsADiv"></div>
-                      <div className="DisplayCardsInBoardsADiv2">
-                        <div className="DisplayCardsInBoardsADiv2Title">{user?.lastBoardVisited[1]?.boardTitle}</div>
-                        <div className="DisplayCardsInBoardsADiv2Svg">
-                          <div className="DisplayCardsInBoardsADiv2SvgIcon">
-                            <div className="DisplayCardsInBoardsADiv2SvgIconDiv">
-                              <span className="DisplayCardsInBoardsADiv2SvgIconSpan">
-                                <svg
-                                  style={{ color: 'white' }}
-                                  viewBox="0 0 24 24"
-                                  focusable="false"
-                                  role="presentation"
-                                  width="16"
-                                  height="16">
-                                  <path
-                                    d="M12.5048 5.67168C11.9099 5.32669 11.2374 5.10082 10.5198 5.0267C11.2076 3.81639 12.5085 3 14 3C16.2092 3 18 4.79086 18 7C18 7.99184 17.639 8.89936 17.0413 9.59835C19.9512 10.7953 22 13.6584 22 17C22 17.5523 21.5523 18 21 18H18.777C18.6179 17.2987 18.3768 16.6285 18.0645 16H19.917C19.4892 13.4497 17.4525 11.445 14.8863 11.065C14.9608 10.7218 15 10.3655 15 10C15 9.58908 14.9504 9.18974 14.857 8.80763C15.5328 8.48668 16 7.79791 16 7C16 5.89543 15.1046 5 14 5C13.4053 5 12.8711 5.25961 12.5048 5.67168ZM10 12C11.1046 12 12 11.1046 12 10C12 8.89543 11.1046 8 10 8C8.89543 8 8 8.89543 8 10C8 11.1046 8.89543 12 10 12ZM14 10C14 10.9918 13.639 11.8994 13.0412 12.5984C15.9512 13.7953 18 16.6584 18 20C18 20.5523 17.5523 21 17 21H3C2.44772 21 2 20.5523 2 20C2 16.6584 4.04879 13.7953 6.95875 12.5984C6.36099 11.8994 6 10.9918 6 10C6 7.79086 7.79086 6 10 6C12.2091 6 14 7.79086 14 10ZM9.99999 14C12.973 14 15.441 16.1623 15.917 19H4.08295C4.55902 16.1623 7.02699 14 9.99999 14Z"
-                                    fill-rule="evenodd"
-                                    clip-rule="evenodd"></path>
-                                </svg>
-                              </span>
+                          <div className="DisplayCardsInBoardsADiv2Svg">
+                            <div className="DisplayCardsInBoardsADiv2SvgIcon">
+                              <div className="DisplayCardsInBoardsADiv2SvgIconDiv">
+                                <span className="DisplayCardsInBoardsADiv2SvgIconSpan">
+                                  <svg
+                                    style={{ color: 'white' }}
+                                    viewBox="0 0 24 24"
+                                    focusable="false"
+                                    role="presentation"
+                                    width="16"
+                                    height="16">
+                                    <path
+                                      d="M12.5048 5.67168C11.9099 5.32669 11.2374 5.10082 10.5198 5.0267C11.2076 3.81639 12.5085 3 14 3C16.2092 3 18 4.79086 18 7C18 7.99184 17.639 8.89936 17.0413 9.59835C19.9512 10.7953 22 13.6584 22 17C22 17.5523 21.5523 18 21 18H18.777C18.6179 17.2987 18.3768 16.6285 18.0645 16H19.917C19.4892 13.4497 17.4525 11.445 14.8863 11.065C14.9608 10.7218 15 10.3655 15 10C15 9.58908 14.9504 9.18974 14.857 8.80763C15.5328 8.48668 16 7.79791 16 7C16 5.89543 15.1046 5 14 5C13.4053 5 12.8711 5.25961 12.5048 5.67168ZM10 12C11.1046 12 12 11.1046 12 10C12 8.89543 11.1046 8 10 8C8.89543 8 8 8.89543 8 10C8 11.1046 8.89543 12 10 12ZM14 10C14 10.9918 13.639 11.8994 13.0412 12.5984C15.9512 13.7953 18 16.6584 18 20C18 20.5523 17.5523 21 17 21H3C2.44772 21 2 20.5523 2 20C2 16.6584 4.04879 13.7953 6.95875 12.5984C6.36099 11.8994 6 10.9918 6 10C6 7.79086 7.79086 6 10 6C12.2091 6 14 7.79086 14 10ZM9.99999 14C12.973 14 15.441 16.1623 15.917 19H4.08295C4.55902 16.1623 7.02699 14 9.99999 14Z"
+                                      fill-rule="evenodd"
+                                      clip-rule="evenodd"></path>
+                                  </svg>
+                                </span>
+                              </div>
                             </div>
+                            <div className="DisplayCardsInBoardsADiv2SvgBlock"></div>
                           </div>
-                          <div className="DisplayCardsInBoardsADiv2SvgBlock"></div>
                         </div>
-                      </div>
-                    </a>
-                  </div>
-                   )}
-                   {user?.lastBoardVisited[2]?.boardTitle && (
-                  <div 
-                    onClick={() => {
-                        const last = user?.lastBoardVisited?.[2];
-
-                        const slug = last?.boardTitle;
-                        const boardId = last?.id;
-
-                        console.log('last visited entry:', last);
-                        console.log('🧠 Navigating to:', `/b/${boardId}/${slug}`);
-
-                        if (boardId && slug) {
-                          navigate(`/b/${boardId}/${slug}`);
-                        }
-                      }}
-                  className="DisplayCardsInBoards">
-                    <a className="DisplayCardsInBoardsA">
-                      <div className="DisplayCardsInBoardsADiv"></div>
-                      <div className="DisplayCardsInBoardsADiv2">
-                        <div className="DisplayCardsInBoardsADiv2Title">{user?.lastBoardVisited[2]?.boardTitle}</div>
-                        <div className="DisplayCardsInBoardsADiv2Svg">
-                          <div className="DisplayCardsInBoardsADiv2SvgIcon">
-                            <div className="DisplayCardsInBoardsADiv2SvgIconDiv">
-                              <span className="DisplayCardsInBoardsADiv2SvgIconSpan">
-                                <svg
-                                  style={{ color: 'white' }}
-                                  viewBox="0 0 24 24"
-                                  focusable="false"
-                                  role="presentation"
-                                  width="16"
-                                  height="16">
-                                  <path
-                                    d="M12.5048 5.67168C11.9099 5.32669 11.2374 5.10082 10.5198 5.0267C11.2076 3.81639 12.5085 3 14 3C16.2092 3 18 4.79086 18 7C18 7.99184 17.639 8.89936 17.0413 9.59835C19.9512 10.7953 22 13.6584 22 17C22 17.5523 21.5523 18 21 18H18.777C18.6179 17.2987 18.3768 16.6285 18.0645 16H19.917C19.4892 13.4497 17.4525 11.445 14.8863 11.065C14.9608 10.7218 15 10.3655 15 10C15 9.58908 14.9504 9.18974 14.857 8.80763C15.5328 8.48668 16 7.79791 16 7C16 5.89543 15.1046 5 14 5C13.4053 5 12.8711 5.25961 12.5048 5.67168ZM10 12C11.1046 12 12 11.1046 12 10C12 8.89543 11.1046 8 10 8C8.89543 8 8 8.89543 8 10C8 11.1046 8.89543 12 10 12ZM14 10C14 10.9918 13.639 11.8994 13.0412 12.5984C15.9512 13.7953 18 16.6584 18 20C18 20.5523 17.5523 21 17 21H3C2.44772 21 2 20.5523 2 20C2 16.6584 4.04879 13.7953 6.95875 12.5984C6.36099 11.8994 6 10.9918 6 10C6 7.79086 7.79086 6 10 6C12.2091 6 14 7.79086 14 10ZM9.99999 14C12.973 14 15.441 16.1623 15.917 19H4.08295C4.55902 16.1623 7.02699 14 9.99999 14Z"
-                                    fill-rule="evenodd"
-                                    clip-rule="evenodd"></path>
-                                </svg>
-                              </span>
-                            </div>
-                          </div>
-                          <div className="DisplayCardsInBoardsADiv2SvgBlock"></div>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                   )}
-                   {user?.lastBoardVisited[3]?.boardTitle && (
-                  <div 
-                    onClick={() => {
-                        const last = user?.lastBoardVisited?.[3];
-
-                        const slug = last?.boardTitle;
-                        const boardId = last?.id;
-
-                        console.log('last visited entry:', last);
-                        console.log('🧠 Navigating to:', `/b/${boardId}/${slug}`);
-
-                        if (boardId && slug) {
-                          navigate(`/b/${boardId}/${slug}`);
-                        }
-                      }}
-                  className="DisplayCardsInBoards">
-                    <a className="DisplayCardsInBoardsA">
-                      <div className="DisplayCardsInBoardsADiv"></div>
-                      <div className="DisplayCardsInBoardsADiv2">
-                        <div className="DisplayCardsInBoardsADiv2Title">{user?.lastBoardVisited[3]?.boardTitle}</div>
-                        <div className="DisplayCardsInBoardsADiv2Svg">
-                          <div className="DisplayCardsInBoardsADiv2SvgIcon">
-                            <div className="DisplayCardsInBoardsADiv2SvgIconDiv">
-                              <span className="DisplayCardsInBoardsADiv2SvgIconSpan">
-                                <svg
-                                  style={{ color: 'white' }}
-                                  viewBox="0 0 24 24"
-                                  focusable="false"
-                                  role="presentation"
-                                  width="16"
-                                  height="16">
-                                  <path
-                                    d="M12.5048 5.67168C11.9099 5.32669 11.2374 5.10082 10.5198 5.0267C11.2076 3.81639 12.5085 3 14 3C16.2092 3 18 4.79086 18 7C18 7.99184 17.639 8.89936 17.0413 9.59835C19.9512 10.7953 22 13.6584 22 17C22 17.5523 21.5523 18 21 18H18.777C18.6179 17.2987 18.3768 16.6285 18.0645 16H19.917C19.4892 13.4497 17.4525 11.445 14.8863 11.065C14.9608 10.7218 15 10.3655 15 10C15 9.58908 14.9504 9.18974 14.857 8.80763C15.5328 8.48668 16 7.79791 16 7C16 5.89543 15.1046 5 14 5C13.4053 5 12.8711 5.25961 12.5048 5.67168ZM10 12C11.1046 12 12 11.1046 12 10C12 8.89543 11.1046 8 10 8C8.89543 8 8 8.89543 8 10C8 11.1046 8.89543 12 10 12ZM14 10C14 10.9918 13.639 11.8994 13.0412 12.5984C15.9512 13.7953 18 16.6584 18 20C18 20.5523 17.5523 21 17 21H3C2.44772 21 2 20.5523 2 20C2 16.6584 4.04879 13.7953 6.95875 12.5984C6.36099 11.8994 6 10.9918 6 10C6 7.79086 7.79086 6 10 6C12.2091 6 14 7.79086 14 10ZM9.99999 14C12.973 14 15.441 16.1623 15.917 19H4.08295C4.55902 16.1623 7.02699 14 9.99999 14Z"
-                                    fill-rule="evenodd"
-                                    clip-rule="evenodd"></path>
-                                </svg>
-                              </span>
-                            </div>
-                          </div>
-                          <div className="DisplayCardsInBoardsADiv2SvgBlock"></div>
-                        </div>
-                      </div>
-                    </a>
-                  </div>
-                   )}
-                </div>
-             
-              
+                      </a>
+                    </div>
+                  );
+                })}
+                
+              </div>
             </div>
             <h3 className="BoardsHomeContainerMainH3">YOUR WORKSPACES</h3>
             <div className="BoardsHomeContainerMainWorkspace">
@@ -414,25 +282,27 @@ export function Boards() {
               </div>
               <div>
                 <ul className="BoardsHomeContainerMainWorkspaceDisplay">
-                  <li 
-                  onClick={() => {
-                        const last = user?.lastBoardVisited?.[0];
+                  <li
+                    onClick={() => {
+                      const last = user?.lastBoardVisited?.[0];
 
-                        const slug = last?.boardTitle;
-                        const boardId = last?.id;
+                      const slug = last?.boardTitle;
+                      const boardId = last?.id;
 
-                        console.log('last visited entry:', last);
-                        console.log('🧠 Navigating to:', `/b/${boardId}/${slug}`);
+                      console.log('last visited entry:', last);
+                      console.log('🧠 Navigating to:', `/b/${boardId}/${slug}`);
 
-                        if (boardId && slug) {
-                          navigate(`/b/${boardId}/${slug}`);
-                        }
-                      }}
-                  className="boards-page-board-section-list-item">
+                      if (boardId && slug) {
+                        navigate(`/b/${boardId}/${slug}`);
+                      }
+                    }}
+                    className="boards-page-board-section-list-item">
                     <a className="board-tile">
                       <span className="board-tile-fade"></span>
                       <div className="board-tile-details">
-                        <h2 className="board-tile-details-name">{user?.lastBoardVisited[0]?.boardTitle}</h2>
+                        <h2 className="board-tile-details-name">
+                          {user?.lastBoardVisited[0]?.boardTitle}
+                        </h2>
                         <div className="board-tile-details-sub-container"></div>
                       </div>
                     </a>
